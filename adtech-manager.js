753ffd0b41e1a892f78427db8302c8adfaa18879
Sindre Gulseth <sgulseth@gmail.com>
2014-11-24 09:31:22 +0100

    Added BC for document.querySelector{,All}

--- adtech-manager.js
+++ adtech-manager.js
@@ -14,6 +14,38 @@
 }(this, function (root) {
     'use strict';
 
+    // Document.querySelectorAll method
+    // http://ajaxian.com/archives/creating-a-queryselector-for-ie-that-runs-at-native-speed
+    // Needed for: IE7-
+    if (!document.querySelectorAll) {
+        document.querySelectorAll = function(selectors) {
+            var style = document.createElement('style'), elements = [], element;
+            document.documentElement.firstChild.appendChild(style);
+            document._qsa = [];
+
+            style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+            window.scrollBy(0, 0);
+            style.parentNode.removeChild(style);
+
+            while (document._qsa.length) {
+                element = document._qsa.shift();
+                element.style.removeAttribute('x-qsa');
+                elements.push(element);
+            }
+            document._qsa = null;
+            return elements;
+        };
+    }
+
+    // Document.querySelector method
+    // Needed for: IE7-
+    if (!document.querySelector) {
+        document.querySelector = function(selectors) {
+            var elements = document.querySelectorAll(selectors);
+            return (elements.length) ? elements[0] : null;
+        };
+    }
+
     // HELPERS
     var slice = Array.prototype.slice;
 
@@ -245,11 +277,15 @@
             }
         },
         hideNotRendered: function() {
-            var positions = document.querySelectorAll('.adtech:not(.ad-loaded)');
+            if (document['querySelectorAll']) {
+                var positions = document.querySelectorAll('.adtech');
 
-            each(positions, function(position) {
-                position.style['display'] = 'none';
-            }, this)
+                each(positions, function(position) {
+                    if (position.className.indexOf('ad-loaded') === -1) {
+                        position.style['display'] = 'none';
+                    }
+                }, this);
+            }
         }
     };
 
