47c3c7d62585a938f17b0e4f563d115a041cb2a2
Sindre Gulseth <sgulseth@gmail.com>
2015-06-01 14:59:27 +0200

    also support blankpix til we have changed all the creatives

--- adtech-manager.js
+++ adtech-manager.js
@@ -254,7 +254,7 @@
             
             if (iframe) {
                 ifDoc = iframe.contentDocument
-                if (ifDoc.getElementById('empty')) {
+                if (ifDoc.getElementById('empty') || ifDoc.querySelector('[id*="blank_pix_house.gif"]')) {
                     el.style['display'] = 'none';
                     el.removeChild(iframe);
                 } else {
